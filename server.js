d6255f2a9caf3911b8d7bae395aca8f1c5a913fe
IGVDev <guidoigv@gmail.com>
2021-12-09 19:00:30 -0300

    [edit]: fix port

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const { connectDb } = require("./config/db");
 const app = require("./config/app");
 connectDb();
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Running on ${server.address().port}`);
 });
