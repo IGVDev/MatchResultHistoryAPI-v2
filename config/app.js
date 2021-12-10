83d1febef5daf5be88e0269f0485aa82673582d0
IGVDev <guidoigv@gmail.com>
2021-12-10 16:45:19 -0300

    [edit]: route file name

--- config/app.js
+++ config/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 // const errorHandler = require("../middlewares/errorHandler");
 const bodyParser = require("body-parser");
-const routes = require("../routes/MatchRoutes");
+const routes = require("../routes/Routes");
 const jsonParser = bodyParser.json();
 const cors = require("cors");
 
--- routes/Routes.js
+++ routes/Routes.js
@@ -3,9 +3,15 @@ const router = express.Router();
 // const errorHandler = require("../middlewares/errorHandler");
 
 const matchController = require("../controllers/MatchController");
+const adminController = require("../controllers/AdminController");
 
 // router.all("*", errorHandler);
 
+// Admin routes
+
+router.post("/admin/login", adminController.adminLogin);
+
+// Match routes
 router.get("/", matchController.baseRoute);
 
 router.post("/createMatch", matchController.createMatch);
